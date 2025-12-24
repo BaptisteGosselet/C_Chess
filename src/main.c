7fedbf8e0ece8fdfd630a1f50c802b9a544f61e8
BaptisteGosselet <baptiste@b-gosselet.localdomain>
2025-12-24 15:23:08 +0100

    🚧 - Separate gui

--- src/main.c
+++ src/main.c
@@ -1,17 +1,7 @@
 #include <stdio.h>
-#include <SDL2/SDL.h>
+#include "gui.h"
 
 int main() {
-    printf("Hello World\n");
-    SDL_Init(SDL_INIT_VIDEO);
-    SDL_Window* win = SDL_CreateWindow("Chess", 100, 100, 640, 640, SDL_WINDOW_SHOWN);
-    SDL_Renderer* ren = SDL_CreateRenderer(win, -1, SDL_RENDERER_ACCELERATED);
-    SDL_SetRenderDrawColor(ren, 255, 255, 255, 255);
-    SDL_RenderClear(ren);
-    SDL_RenderPresent(ren);
-    SDL_Delay(3000);
-    SDL_DestroyRenderer(ren);
-    SDL_DestroyWindow(win);
-    SDL_Quit();
+    open_window();
     return 0;
 }
