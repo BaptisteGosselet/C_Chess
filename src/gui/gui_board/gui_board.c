ad412aa229d1160cd4e8a9135c56fda02fee5a71
BaptisteGosselet <baptiste@b-gosselet.localdomain>
2025-12-24 18:20:53 +0100

    ✨ - drawing pieces

--- src/gui/gui_board/gui_board.c
+++ src/gui/gui_board/gui_board.c
@@ -1,6 +1,23 @@
 #include <SDL2/SDL.h>
 #include <SDL2/SDL_image.h>
 #include "../gui_constants.h"
+#include <stdbool.h>
+
+bool areTexturesLoaded = false;
+
+// Pieces texture
+SDL_Texture *white_pawn_texture   = NULL;
+SDL_Texture *white_rook_texture   = NULL;
+SDL_Texture *white_knight_texture = NULL;
+SDL_Texture *white_bishop_texture = NULL;
+SDL_Texture *white_queen_texture  = NULL;
+SDL_Texture *white_king_texture   = NULL;
+SDL_Texture *black_pawn_texture   = NULL;
+SDL_Texture *black_rook_texture   = NULL;
+SDL_Texture *black_knight_texture = NULL;
+SDL_Texture *black_bishop_texture = NULL;
+SDL_Texture *black_queen_texture  = NULL;
+SDL_Texture *black_king_texture   = NULL;
 
 /**
  * load piece image by filename
@@ -19,7 +36,25 @@ SDL_Texture* load_texture(SDL_Renderer *renderer, const char* pieceName) {
     return texture;
 }
 
-void draw_piece(SDL_Renderer *renderer, SDL_Texture* piece, int x, int y) {
+
+void pieces_textures_init(SDL_Renderer *renderer) {     
+    white_pawn_texture   = load_texture(renderer, PAWN_WHITE_ASSET_NAME);
+    white_rook_texture   = load_texture(renderer, ROOK_WHITE_ASSET_NAME);
+    white_knight_texture = load_texture(renderer, KNIGHT_WHITE_ASSET_NAME);
+    white_bishop_texture = load_texture(renderer, BISHOP_WHITE_ASSET_NAME);
+    white_queen_texture  = load_texture(renderer, QUEEN_WHITE_ASSET_NAME);
+    white_king_texture   = load_texture(renderer, KING_WHITE_ASSET_NAME);
+    black_pawn_texture   = load_texture(renderer, PAWN_BLACK_ASSET_NAME);
+    black_rook_texture   = load_texture(renderer, ROOK_BLACK_ASSET_NAME);
+    black_knight_texture = load_texture(renderer, KNIGHT_BLACK_ASSET_NAME);
+    black_bishop_texture = load_texture(renderer, BISHOP_BLACK_ASSET_NAME);
+    black_queen_texture  = load_texture(renderer, QUEEN_BLACK_ASSET_NAME);
+    black_king_texture   = load_texture(renderer, KING_BLACK_ASSET_NAME);
+    areTexturesLoaded = true;
+}
+
+
+void draw_a_piece(SDL_Renderer *renderer, SDL_Texture* piece, int x, int y) {
     SDL_Rect dest;
     dest.x = x * BOARD_CELL_SIZE;
     dest.y = y * BOARD_CELL_SIZE;
@@ -53,12 +88,32 @@ void draw_board_cells(SDL_Renderer *renderer){
 }
 
 void draw_board(SDL_Renderer *renderer){
+    if(!areTexturesLoaded){
+        pieces_textures_init(renderer);
+    }
     draw_board_cells(renderer);
 
-    SDL_Texture* white_pawn = load_texture(renderer, PAWN_WHITE_ASSET_NAME);
-    SDL_Texture* black_queen = load_texture(renderer, QUEEN_BLACK_ASSET_NAME);
 
-    draw_piece(renderer, white_pawn, 0, 6); 
-    draw_piece(renderer, black_queen, 4, 0);
+    //tmp drawing
+    for(int i=0; i<8;i++){
+            draw_a_piece(renderer, black_pawn_texture, i, 1);
+            draw_a_piece(renderer, white_pawn_texture, i, 6);
+    }
+    draw_a_piece(renderer, black_king_texture, 4, 0);
+    draw_a_piece(renderer, black_queen_texture, 3, 0);
+    draw_a_piece(renderer, white_king_texture, 4, 7);
+    draw_a_piece(renderer, white_queen_texture, 3, 7);
+    draw_a_piece(renderer, black_rook_texture, 0, 0);
+    draw_a_piece(renderer, white_rook_texture, 0, 7);
+    draw_a_piece(renderer, black_rook_texture, 7, 0);
+    draw_a_piece(renderer, white_rook_texture, 7, 7);
+    draw_a_piece(renderer, black_knight_texture, 1, 0);
+    draw_a_piece(renderer, black_knight_texture, 6, 0);
+    draw_a_piece(renderer, white_knight_texture, 6, 7);
+    draw_a_piece(renderer, white_knight_texture, 1, 7);
+    draw_a_piece(renderer, black_bishop_texture, 2, 0);
+    draw_a_piece(renderer, black_bishop_texture, 5, 0);
+    draw_a_piece(renderer, white_bishop_texture, 5, 7);
+    draw_a_piece(renderer, white_bishop_texture, 2, 7);
 
 }
