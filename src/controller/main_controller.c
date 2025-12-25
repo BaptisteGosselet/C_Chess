0cf5be923df982944cd163c159b56949e21010b8
Baptiste Gosselet <91374124+BaptisteGosselet@users.noreply.github.com>
2025-12-25 17:11:14 +0100

    Legal moves (#3)
    * ✨ - simple legall moves
    
    * ✨ - pawn simples moves and capture
    
    * ✨ - pieces simple movements
    
    * ♻️  - legal movves list to arg
    
    * ♻️  - highlight selected cell
    
    * 🚧 - move indicators
    
    * ✨ - roques
    
    ---------
    
    Co-authored-by: BaptisteGosselet <baptiste@b-gosselet.localdomain>

--- src/controller/main_controller.c
+++ src/controller/main_controller.c
@@ -1,12 +1,38 @@
 #include "../gui/gui.h"
 #include "../model/board_model.h"
+#include "../model/move.h"
+#include "../model/legal_move.h"
 #include <stdio.h>
+#include <stdbool.h>
+
 
 int selectedCell[2] = {-1, -1};
 
 void resetSelectedCell(){
     selectedCell[0] = -1;
     selectedCell[1] = -1;
+}
+
+void playSelectedCells(int oI, int oJ, int dI, int dJ){
+    Move wantedMove = (Move){
+            .fromX = oI,
+            .fromY = oJ,
+            .toX = dI,
+            .toY = dJ,
+            .promotion = 0 //todo (if pawn && i/j = 0/8) : ask prom
+        };
+
+    if (!isThisMoveLegal(wantedMove, &currentLegalMovesList)) {
+        printf("Move illegal\n");
+        resetSelectedCell();
+        updateRender();
+        return;
+    }
+
+    moveTo(oI, oJ, dI, dJ);
+    printf("MOVE  %d, %d to %d, %d \n", oI, oJ, dI, dJ);
+    resetSelectedCell();
+    updateRender();
 }
 
 void selectCell(int i, int j){
@@ -26,11 +52,5 @@ void selectCell(int i, int j){
         return;
     }
 
-    //tmp
-    moveTo(selectedCell[0], selectedCell[1], i, j);
-    printf("MOVE  %d, %d to %d, %d \n", selectedCell[0], selectedCell[1], i, j);
-    resetSelectedCell();
-    updateRender();
-    return;
-
+    playSelectedCells(selectedCell[0], selectedCell[1], i, j);
 }
--- src/model/cell.h
+++ src/model/cell.h
@@ -1,9 +1,11 @@
 #ifndef CELL_H
 #define CELL_H
 
+#include "color.h"
+
 // Représente une case de l'échiquier
 typedef struct {
-    int color;   // 0 = blanc, 1 = noir, -1 = vide
+    Color color;   // 0 = blanc, 1 = noir, -1 = vide
     char piece;  // 'p','r','n','b','q','k' ou '\0' pour vide
 } Cell;
 
