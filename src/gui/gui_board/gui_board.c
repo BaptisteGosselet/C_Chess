b4605c2f84d598cd6d78c66342f595f65547b1bd
Baptiste Gosselet <baptiste@b-gosselet.localdomain>
2025-12-27 14:51:12 +0100

    ✨ - flip GUI view

--- src/gui/gui_board/gui_board.c
+++ src/gui/gui_board/gui_board.c
@@ -10,11 +10,41 @@
 
 bool boardClickEnabled = true;
 
+static bool shouldFlipBoard() {
+    const Game *game = controllerGetGame();
+    if (!game) return false;
+    
+    return (game->position.currentColor == COLOR_BLACK && 
+            game->blackPlayerType == PLAYER_HUMAN);
+}
+
+static void logicToDisplay(int logicRow, int logicCol, int *displayRow, int *displayCol) {
+    if (shouldFlipBoard()) {
+        *displayRow = 7 - logicRow;
+        *displayCol = 7 - logicCol;
+    } else {
+        *displayRow = logicRow;
+        *displayCol = logicCol;
+    }
+}
+
+static void displayToLogic(int displayRow, int displayCol, int *logicRow, int *logicCol) {
+    if (shouldFlipBoard()) {
+        *logicRow = 7 - displayRow;
+        *logicCol = 7 - displayCol;
+    } else {
+        *logicRow = displayRow;
+        *logicCol = displayCol;
+    }
+}
 
 void draw_a_piece_on_the_board(SDL_Renderer *renderer, SDL_Texture* piece, int ligne, int col) {
+    int displayRow, displayCol;
+    logicToDisplay(ligne, col, &displayRow, &displayCol);
+    
     SDL_Rect dest;
-    dest.x = BOARD_ORIGIN_X + col * BOARD_CELL_SIZE;
-    dest.y = BOARD_ORIGIN_Y + ligne * BOARD_CELL_SIZE;
+    dest.x = BOARD_ORIGIN_X + displayCol * BOARD_CELL_SIZE;
+    dest.y = BOARD_ORIGIN_Y + displayRow * BOARD_CELL_SIZE;
     dest.w = BOARD_CELL_SIZE;
     dest.h = BOARD_CELL_SIZE;
     SDL_RenderCopy(renderer, piece, NULL, &dest);
@@ -23,10 +53,13 @@ void draw_a_piece_on_the_board(SDL_Renderer *renderer, SDL_Texture* piece, int l
 void draw_highlight_cell(SDL_Renderer *renderer){
     const int *selected = controllerGetSelectedCell();
     if (selected[0] != -1 && selected[1] != -1) {
+        int displayRow, displayCol;
+        logicToDisplay(selected[0], selected[1], &displayRow, &displayCol);
+        
         SDL_SetRenderDrawColor(renderer, 130, 151, 105, 255);
         SDL_Rect rect;
-        rect.x = BOARD_ORIGIN_X + selected[1]*BOARD_CELL_SIZE;   
-        rect.y = BOARD_ORIGIN_Y + selected[0]*BOARD_CELL_SIZE;  
+        rect.x = BOARD_ORIGIN_X + displayCol * BOARD_CELL_SIZE;   
+        rect.y = BOARD_ORIGIN_Y + displayRow * BOARD_CELL_SIZE;  
         rect.w = BOARD_CELL_SIZE;   
         rect.h = BOARD_CELL_SIZE;   
         SDL_RenderFillRect(renderer, &rect);
@@ -38,10 +71,13 @@ void draw_king_check(SDL_Renderer *renderer){
     int kingX = -1, kingY = -1;
     getKingPosition(&pos, pos.currentColor, &kingX, &kingY);
     if (isKingInCheck(&pos, pos.currentColor)){
+        int displayRow, displayCol;
+        logicToDisplay(kingX, kingY, &displayRow, &displayCol);
+        
         SDL_SetRenderDrawColor(renderer, 214, 54, 39, 255);
         SDL_Rect rect;
-        rect.x = BOARD_ORIGIN_X + kingY*BOARD_CELL_SIZE;   
-        rect.y = BOARD_ORIGIN_Y + kingX*BOARD_CELL_SIZE;  
+        rect.x = BOARD_ORIGIN_X + displayCol * BOARD_CELL_SIZE;   
+        rect.y = BOARD_ORIGIN_Y + displayRow * BOARD_CELL_SIZE;  
         rect.w = BOARD_CELL_SIZE;   
         rect.h = BOARD_CELL_SIZE;   
         SDL_RenderFillRect(renderer, &rect);
@@ -49,9 +85,10 @@ void draw_king_check(SDL_Renderer *renderer){
 }
 
 void draw_board_cells(SDL_Renderer *renderer){
-    
     for(int i=0; i<8; i++){
         for(int j=0; j<8; j++){
+            int displayRow, displayCol;
+            logicToDisplay(i, j, &displayRow, &displayCol);
 
             if((i%2) == (j%2)){
                 SDL_SetRenderDrawColor(renderer, 239, 216, 180, 255);
@@ -61,8 +98,8 @@ void draw_board_cells(SDL_Renderer *renderer){
             }
 
             SDL_Rect rect;
-            rect.x = BOARD_ORIGIN_X + i*BOARD_CELL_SIZE;   
-            rect.y = BOARD_ORIGIN_Y + j*BOARD_CELL_SIZE;  
+            rect.x = BOARD_ORIGIN_X + displayCol * BOARD_CELL_SIZE;   
+            rect.y = BOARD_ORIGIN_Y + displayRow * BOARD_CELL_SIZE;  
             rect.w = BOARD_CELL_SIZE;   
             rect.h = BOARD_CELL_SIZE;   
 
@@ -70,7 +107,6 @@ void draw_board_cells(SDL_Renderer *renderer){
         }
     }
 }
-
 
 void draw_board_filter(SDL_Renderer *renderer){
     SDL_Rect filter;
@@ -84,13 +120,11 @@ void draw_board_filter(SDL_Renderer *renderer){
     SDL_RenderFillRect(renderer, &filter);
 }
 
-
 void draw_board_pieces_from_model(SDL_Renderer *renderer) {
     const Position pos = controllerGetGame()->position;
 
     for (int i = 0; i < 8; i++) {
         for (int j = 0; j < 8; j++) {
-
             Cell cell = pos.board_model[i][j];
 
             if (cell.color == COLOR_WHITE) {
@@ -125,7 +159,6 @@ void draw_board_pieces_from_model(SDL_Renderer *renderer) {
     }
 }
 
-
 void draw_board(SDL_Renderer *renderer) {
     if (!getAreTexturesLoaded()) {
         pieces_textures_init(renderer);
@@ -149,11 +182,16 @@ void handle_board_click(SDL_Event *event) {
             int x = event->button.x;
             int y = event->button.y;
 
-            int col = (x - BOARD_ORIGIN_X) / BOARD_CELL_SIZE;
-            int row = (y - BOARD_ORIGIN_Y) / BOARD_CELL_SIZE;
-
-            if (y >= BOARD_ORIGIN_Y && y < (BOARD_ORIGIN_Y+BOARD_SIZE) && x >= BOARD_ORIGIN_X && x < (BOARD_ORIGIN_X+BOARD_SIZE)) {
-                selectCell(row, col);
+            if (y >= BOARD_ORIGIN_Y && y < (BOARD_ORIGIN_Y+BOARD_SIZE) && 
+                x >= BOARD_ORIGIN_X && x < (BOARD_ORIGIN_X+BOARD_SIZE)) {
+                
+                int displayCol = (x - BOARD_ORIGIN_X) / BOARD_CELL_SIZE;
+                int displayRow = (y - BOARD_ORIGIN_Y) / BOARD_CELL_SIZE;
+                
+                int logicRow, logicCol;
+                displayToLogic(displayRow, displayCol, &logicRow, &logicCol);
+                
+                selectCell(logicRow, logicCol);
             }
         }
     }
